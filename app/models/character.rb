d3162e1ca6ef59807db29ac846a352b889c64391
Amanda Mendez <71214594+amandaag39@users.noreply.github.com>
2024-02-14 16:06:28 +0000

    added methods to all model classes

--- app/models/character.rb
+++ app/models/character.rb
@@ -10,4 +10,27 @@
 #  movie_id   :integer
 #
 class Character < ApplicationRecord
+  def movie
+    # Step 1: define the record of the character in question to a variable (1-n showing child to parent)
+    my_movie_id = self.id
+
+    # Step 2: search through the movie table for record with the same character id, this give you all the details
+    matching_movies = Movie.where({ :id => my_movie_id })
+
+    # Step 3: since the above returns a relation, use .at(0) method to return the first record
+    the_movie = matching_movies.at(0)
+
+    # Step 4: return the movie as a result
+    return the_movie
+  end
+
+  def actor
+    my_actor_id = self.id
+
+    matching_actors = Actor.where({ :id => my_actor_id })
+
+    the_actor = matching_actors.at(0)
+
+    return the_actor
+  end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -14,6 +14,7 @@
 #
 class Movie < ApplicationRecord
 
+  #define a method to show 1-1 Director => Movie
   def director
     d_id = self.director_id
 
@@ -22,6 +23,19 @@ class Movie < ApplicationRecord
     the_director = matching_directors.at(0)
 
     return the_director
+  end
+
+  def characters
+    # Step 1: get the id of the record in question from the Movie table (1-n showing parent to child)
+    my_id = self.id
+
+    # Step 2: pull the corresponding record using the Movie id from the Character table
+    matching_characters = Character.where({ :movie_id => my_id })
+
+    # Step 3: return the variable as a result
+    return matching_characters
+
+    # Step 4: create the same relationship in the Character table
   end
 
 end
